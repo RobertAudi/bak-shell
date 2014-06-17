5f5bb93b8c1aa71e56e2b89b3417ac5d2ef14616
Robert Audi <robert.audii@gmail.com>
2014-06-17 23:01:55 +0200

    (Specific) Backups can now be removed

---
+++ lib/bak-shell/commands/purge.rb
@@ -0,0 +1,31 @@
+module BakShell
+  class CLI
+    desc "Remove (specific) backups"
+    arg_name "TARGET"
+    command :purge do |c|
+      c.action do |global_options, options, args|
+        indexer = Indexer.instance
+        targets = Array.new
+
+        if args.empty?
+          targets = Dir.glob(File.join(BakShell::BACKUP_DIR, "*"))
+          targets.delete(indexer.index_file)
+        else
+          args.each do |arg|
+            target = File.expand_path(arg)
+            raise ArgumentError, "No such file or directory: #{target}" unless File.exists?(target)
+
+            backup = indexer.backup_with_target(target)
+
+            # TODO: Show some kind of warning
+            next if backup.nil?
+
+            targets << File.join(BakShell::BACKUP_DIR, backup.id)
+          end
+        end
+
+        targets.each { |t| FileUtils.rm_r(t, force: true) }
+      end
+    end
+  end
+end
